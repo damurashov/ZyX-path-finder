39f63727cb5b94dd648893dff19785a5e029314f
sleeplynerd <dmitry.murzon@gmail.com>
2017-07-14 21:16:36 +0400

    map_2d.h - DONE

--- src/map_2d.h
+++ src/map_2d.h
@@ -6,35 +6,67 @@
 
 
 class Map2d {
-public:
-
-    struct Coord;
-
-    std::list<Coord> getPath( Coord start, Coord dest ) const;
-
-    void addObstacle( Coord coord );
-
-    void setCost( Coord coord, unsigned short cost );
-
-    Map2d(
-        int xSize,
-        int ySize,
-        unsigned short cost );
-
 private:
-    static const int OBSTACLE = -1;
+    enum class Direction { N, NE, E, SE, S, SW, W, NW };
 
-    enum class Direction { N, NE, E, SE, S, SW, W, NW }
     class Node;
 
+    static const int OBSTACLE = -1;
     const int X_SIZE;
     const int Y_SIZE;
 
     Map2d();
 
+public:
+
+    /**
+     * X, Y and some arithmetic operators
+     */
+    struct Coord;
+
+
+    /**
+     * Returns the path.
+     * @param  start Start point
+     * @param  dest  Destination point
+     * @return       Returns the seeking path as a list.
+     *                       Direct order:
+     *                       getPath(...).begin() - point "from"
+     *                       getPath(...).end() - point "to"
+     *                       Example of usage: http://www.cplusplus.com/reference/list/
+     */
+    std::list<Coord> getPath( Coord start, Coord dest ) const;
+
+
+    /**
+     * Adds an obstacle.
+     * @param coord Coordinates of the obstacle.
+     */
+    void addObstacle( const Coord& coord );
+
+
+    /**
+     * Provides an opportunity to set your own cost to a point. The higher value
+     *   has been set, the less proirity the point has.
+     * @param coord Coordinates
+     * @param cost  Cost
+     */
+    void setCost( Coord coord, unsigned short cost );
+
+
+    /**
+     * Constructor
+     * @param xSize X-axis-size of the field
+     * @param ySize Y-axis-size of the field
+     * @param cost  Default cost. It will be set for each point.
+     */
+    Map2d(
+        int xSize,
+        int ySize,
+        unsigned short cost );
+
 };
 
-//Coord operator+(  )
 
 struct Map2d::Coord {
     int x;
@@ -51,20 +83,54 @@ struct Map2d::Coord {
 
 class Map2d::Node {
 public:
+    /**
+     * Changes mDest to the current destination point.
+     * @param dest Coordinates of the destination point
+     */
     static void notifyDest( Coord dest );
+
+
+    /**
+     * Algorithm routine.
+     *   See: https://en.wikipedia.org/wiki/A*_search_algorithm
+     * @param parent Parental node
+     */
     void setParent( const Node& parent );
-    void setUserCost( const short& cost = );
+
+
+    /**
+     * See Map2d::setCost(...)
+     * @param cost The cost value.
+     */
+    void setUserCost( const short& cost );
+
+
+    /**
+     * @return Returns a refer to a parent node.
+     */
     Node& getParent();
+
+
+    /**
+     * Algorithm routine.
+     * @return Returns f(x) = h(x) + g(x)
+     */
     int getHeuristic();
+
+
+    /**
+     * @param coord Coordinates of the current node.
+     */
     Node( Coord coord );
 
-    //int getUserCost();
 private:
     const Coord     COORD;
     static Coord    mDest;
     Node*           mParent;
     int             mHeuristic;
     unsigned short  mUserCost;
+
+    Node();
 };
 
 #endif /* end of include guard: MAP_2D_H */
